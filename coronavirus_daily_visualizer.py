6ecec85bceb909695d22e8e7ca93bdf1e796ca38
Sumit Bahl <bahl.sumitbahl@gmail.com>
2020-03-14 22:04:32 -0400

    Few fixes and updated with latest run

--- coronavirus_daily_visualizer.py
+++ coronavirus_daily_visualizer.py
@@ -13,17 +13,13 @@ df_sum = df
 #uniq = df['Country/Region'].unique()
 uniq = ['US']
 df_final = pd.DataFrame()
-number_of_days = 4
+number_of_days = 5
 
 for country in uniq:
     country_code = df['Country/Region']==country
     df_coutry = df[country_code]
     df_coutry = df_coutry.sort_values(by=[today], ascending=False)
     df_coutry = df_coutry.head(10)
-
-    #print(df_coutry)
-
-    # df_coutry['Province/State'] = df_coutry['Province/State'].str.split(', ').str[1]
     
     column_list = list(df_coutry)
     column_list.remove('Country/Region')
@@ -39,7 +35,13 @@ for country in uniq:
     print(dfT)
     column_list_T = list(dfT)
     column_list_T.remove('date')
-    
+
+    column_val = {}
+    for column in column_list_T:
+        column_val[column] = dfT[column][today]
+
+    sorted_column_val = sorted(column_val.items(), key=lambda x: x[1], reverse=True)
+
     colors = dict(mcolors.BASE_COLORS, **mcolors.CSS4_COLORS)
     colors_to_draw = []
 
@@ -47,7 +49,11 @@ for country in uniq:
         colors_to_draw.append(color)
 
     index = 0
-    for column in column_list_T:
+    final_column_list = []
+    for i in range(len(sorted_column_val)):
+        final_column_list.append(sorted_column_val[i][0])
+    
+    for column in final_column_list:
         plt.plot( 'date' , column, data=dfT, marker='', color=colors_to_draw[index], linewidth=2, label=column)
         plt.legend()
         index = index + 1
